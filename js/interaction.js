811e9d5cfc0915de78627c0607d69fbce3231bcb
Steven Albert <steven.albert2001@gmail.com>
2021-12-12 01:44:23 +0200

    WALLS ATTEMPT 1

--- js/interaction.js
+++ js/interaction.js
@@ -2,13 +2,13 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module
 
 let object, intersects;
 
-function interact(e, raycaster2, interactables, camera){
+function interact(e, raycaster, interactables, camera){
   var cursor = new THREE.Vector2(0,0);  
   console.log(cursor);
-  raycaster2.setFromCamera(cursor, camera);
-  intersects = raycaster2.intersectObjects(interactables, true);
+  raycaster.setFromCamera(cursor, camera);
+  intersects = raycaster.intersectObjects(interactables, true);
   if(intersects.length>0){
-  intersects[0].object.material.color.set(Math.random() * 0xffffff);
+   intersects[0].object.material.color.set(Math.random() * 0xffffff);
 }
   console.log(intersects[0]);
   return intersects[0].point;
